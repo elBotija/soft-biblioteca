3d999865eb9c5d13aa9d8ec45b7f6afcbb35250a
Mauro Martinez <mmartinez@edrans.com>
2019-06-17 22:49:48 -0300

    listado de suscriptores

--- src/compoenents/suscriptors/Suscriptores.js
+++ src/compoenents/suscriptors/Suscriptores.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { Link } from 'react-router-dom'
 
-const Suscriptores = () => {
+const Suscriptores = ({suscriptores}) => {
+    if(!suscriptores) return <h1>Cargando...</h1>
     return ( 
-        <h1>Suscriptores</h1>
+        <div className="row">
+            <div className="col-md-12 mb-4">
+                <Link to={'/suscriptores/nuevo'} className="btn btn-primary"> <i className="fas fa-plus mr-2"></i>Nuevo suscriptor</Link>
+            </div>
+            <div className="col-md-8">
+                <h2>
+                    <i className="fas fa-users mr-2"></i>Suscriptores
+                </h2>
+            </div>
+            <table className="table table-striped mt-4">
+                <thead className="text-light bg-primary">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Carrera</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {suscriptores.map(s => (
+                        <tr key={s.id}>
+                            <td>{s.nombre} {s.apellido}</td>
+                            <td>{s.carrera}</td>
+                            <td>
+                                <Link 
+                                    to={`/suscriptores/mostrar/${s.id}`}
+                                    className="btn btn-success btn-block"
+                                >
+                                    <i className="fas fa-angle-double-right mr-2"></i>
+                                    Más info
+                                </Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
      );
 }
  
-export default Suscriptores;
+export default compose(
+    firestoreConnect([{ collection : 'suscriptores' }]),
+    connect((state, props)=>({
+      suscriptores : state.firestore.ordered.suscriptores
+    }))
+)(Suscriptores);
