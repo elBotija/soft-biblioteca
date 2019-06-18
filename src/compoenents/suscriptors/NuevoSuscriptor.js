927411b076d7ad9fb9033aea06a813535b2f77e1
Mauro Martinez <mmartinez@edrans.com>
2019-06-18 17:47:25 -0300

    avances

--- src/compoenents/suscriptors/NuevoSuscriptor.js
+++ src/compoenents/suscriptors/NuevoSuscriptor.js
@@ -1,11 +1,93 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class NuevoSuscriptor extends Component {
-    state = {  }
+    state = { 
+        nombre: '',
+        apellido: '',
+        carrera: '',
+        codigo: ''
+    }
+
+    _handlerInput(e){
+        e.preventDefault()
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render() { 
         return ( 
-            <h1>Nuevo Suscriptor</h1>
-         );
+            <div className="row">
+                <div className="col-12 mb-4">
+                    <Link to={'/suscriptores'} className="btn btn-secondary">
+                        <i className="fas fa-arrow-circle-left mr-2"></i>
+                        Volver al listado
+                    </Link>
+                </div>
+                <div className="col-12">
+                    <h2>
+                        <i className="fas fa-user-plus mr-2"></i>
+                        Nuevo Suscroptor
+                    </h2>
+                    <div className="row justify-content-center">
+                        <div className="col-md-8 mt-5">
+                            <form>
+                                <div className="form-group">
+                                    <label>Nombre:</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        name="nombre"
+                                        placeholder="Nombre del Suscriptor"
+                                        required
+                                        onChange={(e)=>this._handlerInput(e)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Apellido:</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        name="apellido"
+                                        placeholder="Apellido del Suscriptor"
+                                        required
+                                        onChange={(e)=>this._handlerInput(e)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Carrera:</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        name="carrera"
+                                        placeholder="Carrera del Suscriptor"
+                                        required
+                                        onChange={(e)=>this._handlerInput(e)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label>Codigo:</label>
+                                    <input 
+                                        type="text"
+                                        className="form-control"
+                                        name="codigo"
+                                        placeholder="Codigo del Suscriptor"
+                                        required
+                                        onChange={(e)=>this._handlerInput(e)}
+                                    />
+                                </div>
+                                <input 
+                                    type="submit"
+                                    value="Agregar Suscriptor"
+                                    className="btn btn-success"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
  
