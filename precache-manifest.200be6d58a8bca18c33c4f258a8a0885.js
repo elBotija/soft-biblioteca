608716c1699e555da87ecf03fe3b54d1c8205817
Mauro Martinez <mmartinez@edrans.com>
2019-08-19 23:20:25 -0300

    Updates

---
+++ precache-manifest.200be6d58a8bca18c33c4f258a8a0885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecbfea858a42654cda33394fd1f9ed6",
+    "url": "/soft-biblioteca/index.html"
+  },
+  {
+    "revision": "dfe3bd03fe6fbc62299c",
+    "url": "/soft-biblioteca/static/css/main.b051aae4.chunk.css"
+  },
+  {
+    "revision": "e98bd4129ab35f73fc98",
+    "url": "/soft-biblioteca/static/js/2.f6a39727.chunk.js"
+  },
+  {
+    "revision": "dfe3bd03fe6fbc62299c",
+    "url": "/soft-biblioteca/static/js/main.e1009306.chunk.js"
+  },
+  {
+    "revision": "5d8a8c77e038e30f8b35",
+    "url": "/soft-biblioteca/static/js/runtime~main.82bea215.js"
+  }
+]);
